42437f9f0ecefb8139c2045bf2abddf722598c6b
Yaroslav Davydchuk <d.yarik@gmail.com>
2024-05-13 17:01:29 +0100

    add blue line

--- bot.js
+++ bot.js
@@ -1,17 +1,24 @@
 const {Bot} = require("grammy");
+
 dotenv = require('dotenv');
 console.log(process.env.NODE_ENV)
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 
 const bot = new Bot(process.env.BOT_TOKEN);
+const mainController = require("./controller/controller");
+const { stateSavingMiddleware } = require('./controller/botMiddleware')
 
+//bot middlewares
 
-bot.command("start", (ctx) => ctx.reply("Ласкаво просимо! Бот запущений."));
-bot.on("message", async (ctx) => {
-    await ctx.reply("Отримав ще одне повідомлення!");
-    console.log(ctx.from.id);
-    await bot.api.sendMessage(ctx.from.id, `Привіт, ${ctx.from.id}`);
+bot.use(stateSavingMiddleware());
+
+bot.command("test", (ctx) => mainController.controller('testMessage')(ctx));
+
+bot.command("start", (ctx) => mainController.controller('startMessage')(ctx));
+bot.on("message", (ctx) => mainController.controller('defaultMessage')(ctx));
+bot.on("callback_query", (ctx) => {
+    mainController.controller(ctx.callbackQuery.data)(ctx)
 });
 
 
