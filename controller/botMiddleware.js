14348d509037a1dd5fd82c54a1439f536807a6ae
Yaroslav Davydchuk <d.yarik@gmail.com>
2024-05-14 15:22:54 +0100

    refactor stateSavingMiddleware, change error handling

--- controller/botMiddleware.js
+++ controller/botMiddleware.js
@@ -1,24 +1,19 @@
 const { saveUserState } = require('../db/stateManager');
 
 function stateSavingMiddleware() {
-    return (ctx, next) => {
-        console.log('CONTEXT:', ctx.callbackQuery?.data || ctx.message?.text)
-        if (ctx.callbackQuery?.data) {
-            saveUserState(ctx.from.id, ctx.from.username, ctx.callbackQuery.data)
-                .then(() => console.log("1. State saved successfully"))
-                .catch(err => console.error("1. Error saving state", err));
-        } else if (ctx.message?.text) {
-            saveUserState(  ctx.from.id,
-                            ctx.from.username,
-                            ctx.message.text === '/start'
-                                ? '/start'
-                                : 'txt')
-                .then(() => console.log("2. State saved successfully"))
-                .catch(err => console.error("2. Error saving state", err));
+    return async (ctx, next) => {
+        try {
+            console.log('📥 CONTEXT:', ctx.callbackQuery?.data || ctx.message?.text);
+
+            let data = ctx.callbackQuery?.data || (ctx.message?.text === '/start' ? '/start' : 'txt' || 'ERROR');
+            await saveUserState(ctx.from.id, ctx.from.username, data);
+            console.log("👌 State saved successfully");
+        } catch (err) {
+            console.error("🧨 Error saving state:", err.message);
+            // ToDo: save error to log
         }
         next();
     }
 }
-
 
 module.exports = { stateSavingMiddleware };
