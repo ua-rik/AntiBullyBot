f9ffb3d5283355976f57a13471dcf5a8d1c0a3d2
Yaroslav Davydchuk <d.yarik@gmail.com>
2024-05-17 13:34:29 +0100

    add gender handler

--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,14 @@
 const { msg } = require('../conversations/allMessages')
+const { getUserGender } = require('../db/stateManager');
 
-exports.getLastMessage = async (ctx) => {
+const getLastMessage = async (ctx) => {
     const lastMessageId = ctx.callbackQuery?.data.split('/')[1] || null
-    const lastMessage = msg[lastMessageId]
 
+    const lastMessage = msg[lastMessageId]
+    const genderReadyLastMessage = await genT(lastMessage, ctx)
     try {
-        if (lastMessage) {
-            await ctx.editMessageText(lastMessage,
+        if (genderReadyLastMessage) {
+            await ctx.editMessageText(genderReadyLastMessage,
                 {
                     parse_mode: "HTML",
                     disable_web_page_preview: true,
@@ -17,3 +19,26 @@ exports.getLastMessage = async (ctx) => {
         return null;
     }
 };
+
+// обробляє повідомлення перед вставкою
+const genT = async (message, ctx) => {
+    // Перевірка наявності гендерних тегів у тексті
+    if (/{.*?\|.*?}/.test(message)) {
+        // Отримання статі користувача з бази даних
+        const gender = await getUserGender(ctx.from.id);
+        // Заміна гендерних тегів на відповідні форми
+        return replaceGenderSpecificText(message, gender);
+    } else {
+        // Якщо гендерних тегів немає, повертаємо текст без змін
+        return message;
+        }
+}
+
+// Функція для заміни гендерних тегів у тексті
+function replaceGenderSpecificText(text, gender) {
+    return text.replace(/{(.*?)\|(.*?)}/g, (match, maleForm, femaleForm) => {
+        return gender === 'female' ? femaleForm : maleForm;
+    });
+}
+
+module.exports = { getLastMessage, genT };
